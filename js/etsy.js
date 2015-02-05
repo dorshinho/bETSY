985eeda389a1a1aff78d28209045d6ba00a490c3
Dorsh <dorshdeans@gmail.com>
2015-02-05 14:51:48 -0500

    search bar working

--- js/etsy.js
+++ js/etsy.js
@@ -17,7 +17,6 @@
             details: function(id) {
                 self.drawDetails(id)
 
-                // ... self.drawDetails(id)
             },
             initialize: function() {
                 Backbone.history.start();
@@ -43,26 +42,25 @@
 
             // 3. write to DOM
             $.when(etsyPromise, templatePromise).then(function(data, html) {
-                var templatingFn = _.template(html)
-                document.body.innerHTML = templatingFn(data)
+                var templatingFn = _.template(html);
+                document.querySelector(".container").innerHTML = templatingFn(data);
             })
         },
         drawDetails: function(id) {
             // 1. setup url
             var url = "https://openapi.etsy.com/v2/listings/" + id + ".js?includes=Images&api_key=" + this.api_key + "&callback=?";
-            console.log(url)
 
 
             var etsyPromise = $.getJSON(url).then(function(d) {
                 return d
             })
-            var template = "./templates/search.html"
+            var template = "./templates/details.html"
             var templatePromise = $.get(template).then(function(d) {
                 return d
             })
             $.when(etsyPromise, templatePromise).then(function(data, html) {
-                var templatingFn = _.template(html)
-                document.body.innerHTML = templatingFn(data)
+                var templatingFn = _.template(html);
+            document.querySelector(".container").innerHTML = templatingFn(data);
             })
         },
 
